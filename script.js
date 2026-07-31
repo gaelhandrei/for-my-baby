8150b9f09baf27e1a4a1094deae878d4840ff62a
gaelhandrei <gaelhandrei2306@gmail.com>
2026-07-31 23:30:21 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const message = Hello Baby, 
+Happy National Girlfriend Day <3
+Thank you for always choosing me, every single day.
+Thank you for staying by my side through the good days and even the hard ones.
+Thank you for loving me, understanding me, and never giving up on us.
+Every smile, every laugh, every hug, and every memory we have made together means so much to me. 
+I will always treasure every moment with you.
+You are my safe place, my happiness, and the best thing thats ever happened to me. 
+You are my beautiful baby... my glorious queen
+I will always choose you, love you, and appreciate you with all my heart.
+I love you more than words could ever explain baby, and I will keep loving you every single day.
+Forever yours,
+Your glorious king<3';
+  
+let i = 0:
+
+function typeWriter()}
+  if(i < message.length)}
+    document.getElementById("typing").innerHTML += message.charAt(i);
+    i++;
+    setTimeout(typeWriter, 45);
+  }
+}
+
+window.unload = typeWriter;
